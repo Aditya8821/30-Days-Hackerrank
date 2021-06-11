63fc6ea6e511d0bbf7bd5836e64d191b417d5ed5
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2021-06-11 10:23:20 +0530

    Create Day_7_Arrays.py

---
+++ Day_7_Arrays.py
@@ -0,0 +1,11 @@
+import math
+import os
+import random
+import re
+import sys
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    arr.reverse()
+    for i in arr:
+        print(i,end=" ")
